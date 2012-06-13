53f3825c00393ab4a5f26e02f19bd2db15e026ec
Dennis Ruban <y_denni@yhaoo.co.in>
2012-06-13 15:05:07 +0530

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 SampleApp1::Application.routes.draw do
+  get "users/new"
+
   # get "pages/home", :to => 'pages#home'
   
   # match '/', :to => 'pages#home'
 
+
+  root :to => 'pages#home'
+  
+   match '/signup', :to => 'users#new'
+  
   match '/contact', :to => 'pages#contact'
   
   match '/about', :to => 'pages#about'
   
   match '/help', :to => 'pages#help'
   
-  root :to => 'pages#home'
+ 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  it "should have a Home page at'/" do
+    get '/'
+    responce.should have_selector('title', :content => "Home")
+  end
+  
+    it "should have a Contact page at'/" do
+    get '/'
+    responce.should have_selector('title', :content => "Contact")
+  end
+  
+  
+    it "should have a About page at'/" do
+    get '/'
+    responce.should have_selector('title', :content => "About")
+  end
+  
+    it "should have a Help page at'/" do
+    get '/'
+    responce.should have_selector('title', :content => "Help")
+  end
+        
+      
+    it "should have a Sign up page at'/signup" do
+    get '/'
+    responce.should have_selector('title', :content => "Sign up")
+  end
+  
+  describe "GET /layout_links" do
+    it "works! (now write some real specs)" do
+      get layout_links_path
+    end
+  end
+end
